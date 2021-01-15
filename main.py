2236740987eccece3fe9ceb4a28f02a4ce6467bd
Dyaj-Java <dyaj@rediffmail.com>
2021-01-16 03:47:02 +0530

    fixed url mapping

--- main.py
+++ main.py
@@ -68,6 +68,6 @@ class main(Resource):
         return list(map(lambda x: x.json(), Student.query.all()))
 
 
-api.add_resource(main, '/all')
+api.add_resource(main, '/mcit/cst-students/all')
 if __name__ == '__main__':
     app.run()
